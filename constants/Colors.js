ea46e8ff2b862650d2dca7c1245020e6af6198b0
Bhuwan chettri <bhuwanchettri@neputer.com>
2023-02-19 17:13:05 +0545

    first commit

---
+++ constants/Colors.js
@@ -0,0 +1,17 @@
+export default colors = {
+  primary: "#FB6831",
+  primary_light: "#FFC8B2",
+  primary_shadow: "#FB6A04",
+  secondary: "#312783",
+  button: "#5EC9ED",
+  tertiary: "#AEE8FD",
+  success: "#90ee90",
+  danger: "#FF4848",
+  shadow: "#E7E8EA",
+  warning: "#FBD431",
+  info: "#F8F9FA",
+  light: "#F5F5F5",
+  dark: "#343A3F",
+  muted: "#707981",
+  white: "#FFFFFF",
+};
